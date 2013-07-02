ef86eb725ced4c056c57cb60eca468bbbbde2073
Douglas Petronilio <dougpetronilio@gmail.com>
2013-07-02 01:04:37 -0300

    Comecando a aplicacao de integracao com pipedrive.

---
+++ app/controllers/lead_controller.rb
@@ -0,0 +1,7 @@
+class LeadController < ApplicationController
+  def list
+  end
+
+  def create
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webrdspdrive::Application.config.secret_token = '013389233ac1817bcfffbf933bd7b5ad199ad334e4a58f31a481a6c383a61bd2892d5b319ef34e4cfee42b0b9abfd8c0a8ecb31ef32ca0c05d648daf6372d27c'
